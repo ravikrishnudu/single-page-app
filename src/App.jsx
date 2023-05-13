a4f6436b9ad8085ced94a375f547352ccb44cc0f
Ravi krishnudu <ravikrishnuduvena@gmail.com>
2023-05-13 20:40:07 +0530

    Remove text

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,6 @@ function Layout() {
           so you can think about this <Outlet> as a placeholder for
           the child routes we defined above. */}
       <Outlet />
-      Nothing to see here!
     </div>
   );
 }
